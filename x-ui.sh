d9129347d1dbefe1fb29d2f92d603f6e659ce387
sihan kumara anushka <106658675+sihankumar@users.noreply.github.com>
2022-06-02 11:08:59 +0530

    Update x-ui.sh
    fdgbh

--- x-ui.sh
+++ x-ui.sh
@@ -6,7 +6,7 @@ yellow='\033[0;33m'
 plain='\033[0m'
 
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
+[[ $EUID -ne 0 ]] &amp;&amp; echo -e "${red}Error: ${plain} must be root to run this script!\n" &amp;&amp; exit 1
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -24,7 +24,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+    echo -e "${red} no system version detected, please contact the script author! ${plain}\n" &amp;&amp; exit 1
 fi
 
 os_version=""
@@ -33,27 +33,27 @@ os_version=""
 if [[ -f /etc/os-release ]]; then
     os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
 fi
-if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+if [[ -z "$os_version" &amp;&amp; -f /etc/lsb-release ]]; then
     os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
 fi
 
 if [[ x"${release}" == x"centos" ]]; then
     if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Please use CentOS 7 or later! ${plain}\n" &amp;&amp; exit 1
     fi
 elif [[ x"${release}" == x"ubuntu" ]]; then
     if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Please use Ubuntu 16 or later! ${plain}\n" &amp;&amp; exit 1
     fi
 elif [[ x"${release}" == x"debian" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
+        echo -e "${red}Please use Debian 8 or later! ${plain}\n" &amp;&amp; exit 1
     fi
 fi
 
 confirm() {
-    if [[ $# > 1 ]]; then
-        echo && read -p "$1 [默认$2]: " temp
+    if [[ $# &gt; 1 ]]; then
+        echo &amp;&amp; read -p "$1 [default $2]: " temp
         if [[ x"${temp}" == x"" ]]; then
             temp=$2
         fi
@@ -68,7 +68,7 @@ confirm() {
 }
 
 confirm_restart() {
-    confirm "是否重启面板，重启面板也会重启 xray" "y"
+    confirm "Whether to restart the panel, restarting the panel will also restart xray" "y"
     if [[ $? == 0 ]]; then
         restart
     else
@@ -77,12 +77,12 @@ confirm_restart() {
 }
 
 before_show_menu() {
-    echo && echo -n -e "${yellow}按回车返回主菜单: ${plain}" && read temp
+    echo &amp;&amp; echo -n -e "${yellow} press enter to return to the main menu: ${plain}" &amp;&amp; read temp
     show_menu
 }
 
 install() {
-    bash <(curl -Ls https://blog.sprov.xyz/x-ui.sh)
+    bash &lt;(curl -Ls https://blog.sprov.xyz/x-ui.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
@@ -93,23 +93,23 @@ install() {
 }
 
 update() {
-    confirm "本功能会强制重装当前最新版，数据不会丢失，是否继续?" "n"
+    confirm "This function will force the latest version to be reinstalled without losing data. Do you want to continue?" "n"
     if [[ $? != 0 ]]; then
-        echo -e "${red}已取消${plain}"
+        echo -e "${red} canceled ${plain}"
         if [[ $# == 0 ]]; then
             before_show_menu
         fi
         return 0
     fi
-    bash <(curl -Ls https://raw.githubusercontent.com/sprov065/x-ui/master/install.sh)
+    bash &lt;(curl -Ls https://raw.githubusercontent.com/sprov065/x-ui/master/install.sh)
     if [[ $? == 0 ]]; then
-        echo -e "${green}更新完成，已自动重启面板${plain}"
+        echo -e "${green} update completed, the panel has been automatically restarted ${plain}"
         exit 0
     fi
 }
 
 uninstall() {
-    confirm "确定要卸载面板吗，xray 也会卸载?" "n"
+    confirm "Are you sure you want to uninstall the panel, xray will also be uninstalled?" "n"
     if [[ $? != 0 ]]; then
         if [[ $# == 0 ]]; then
             show_menu
@@ -125,11 +125,11 @@ uninstall() {
     rm /usr/local/x-ui/ -rf
 
     echo ""
-    echo -e "卸载成功，如果你想删除此脚本，则退出脚本后运行 ${green}rm /usr/bin/x-ui -f${plain} 进行删除"
+    echo -e "Uninstalled successfully, if you want to delete this script, run ${green}rm /usr/bin/x-ui -f${plain} to delete it after exiting the script"
     echo ""
-    echo -e "Telegram 群组: ${green}https://t.me/sprov_blog${plain}"
+    echo -e "Telegram group: ${green}https://t.me/sprov_blog${plain}"
     echo -e "Github issues: ${green}https://github.com/sprov065/x-ui/issues${plain}"
-    echo -e "博客: ${green}https://blog.sprov.xyz/x-ui${plain}"
+    echo -e "Blog: ${green}https://blog.sprov.xyz/x-ui${plain}"
 
     if [[ $# == 0 ]]; then
         before_show_menu
@@ -137,77 +137,14 @@ uninstall() {
 }
 
 reset_user() {
-    confirm "确定要将用户名和密码重置为 admin 吗" "n"
+    confirm "Are you sure you want to reset username and password to admin" "n"
     if [[ $? != 0 ]]; then
-        if [[ $# == 0 ]]; then
-            show_menu
-        fi
-        return 0
-    fi
-    /usr/local/x-ui/x-ui setting -username admin -password admin
-    echo -e "用户名和密码已重置为 ${green}admin${plain}，现在请重启面板"
-    confirm_restart
-}
-
-reset_config() {
-    confirm "确定要重置所有面板设置吗，账号数据不会丢失，用户名和密码不会改变" "n"
-    if [[ $? != 0 ]]; then
-        if [[ $# == 0 ]]; then
-            show_menu
-        fi
-        return 0
-    fi
-    /usr/local/x-ui/x-ui setting -reset
-    echo -e "所有面板设置已重置为默认值，现在请重启面板，并使用默认的 ${green}54321${plain} 端口访问面板"
-    confirm_restart
-}
-
-set_port() {
-    echo && echo -n -e "输入端口号[1-65535]: " && read port
-    if [[ -z "${port}" ]]; then
-        echo -e "${yellow}已取消${plain}"
-        before_show_menu
-    else
-        /usr/local/x-ui/x-ui setting -port ${port}
-        echo -e "设置端口完毕，现在请重启面板，并使用新设置的端口 ${green}${port}${plain} 访问面板"
-        confirm_restart
-    fi
-}
-
-start() {
-    check_status
-    if [[ $? == 0 ]]; then
-        echo ""
-        echo -e "${green}面板已运行，无需再次启动，如需重启请选择重启${plain}"
-    else
-        systemctl start x-ui
-        sleep 2
-        check_status
-        if [[ $? == 0 ]]; then
-            echo -e "${green}x-ui 启动成功${plain}"
-        else
-            echo -e "${red}面板启动失败，可能是因为启动时间超过了两秒，请稍后查看日志信息${plain}"
-        fi
-    fi
-
-    if [[ $# == 0 ]]; then
-        before_show_menu
-    fi
-}
-
-stop() {
-    check_status
-    if [[ $? == 1 ]]; then
-        echo ""
-        echo -e "${green}面板已停止，无需再次停止${plain}"
-    else
-        systemctl stop x-ui
-        sleep 2
-        check_status
+        if 
+        eck_status
         if [[ $? == 1 ]]; then
-            echo -e "${green}x-ui 与 xray 停止成功${plain}"
+            echo -e "${green}x-ui and xray stopped successfully ${plain}"
         else
-            echo -e "${red}面板停止失败，可能是因为停止时间超过了两秒，请稍后查看日志信息${plain}"
+            echo -e "${red} panel failed to stop, maybe because the stop time exceeded two seconds, please check the log information later ${plain}"
         fi
     fi
 
@@ -221,9 +158,9 @@ restart() {
     sleep 2
     check_status
     if [[ $? == 0 ]]; then
-        echo -e "${green}x-ui 与 xray 重启成功${plain}"
+        echo -e "${green}x-ui and xray restarted successfully ${plain}"
     else
-        echo -e "${red}面板重启失败，可能是因为启动时间超过了两秒，请稍后查看日志信息${plain}"
+        echo -e "${red} panel failed to restart, maybe because the startup time exceeded two seconds, please check the log information later ${plain}"
     fi
     if [[ $# == 0 ]]; then
         before_show_menu
@@ -240,9 +177,9 @@ status() {
 enable() {
     systemctl enable x-ui
     if [[ $? == 0 ]]; then
-        echo -e "${green}x-ui 设置开机自启成功${plain}"
+        echo -e "${green}x-ui set boot up successfully ${plain}"
     else
-        echo -e "${red}x-ui 设置开机自启失败${plain}"
+        echo -e "${red}x-ui failed to set boot auto-start ${plain}"
     fi
 
     if [[ $# == 0 ]]; then
@@ -253,9 +190,9 @@ enable() {
 disable() {
     systemctl disable x-ui
     if [[ $? == 0 ]]; then
-        echo -e "${green}x-ui 取消开机自启成功${plain}"
+        echo -e "${green}x-ui canceled boot auto-start successfully ${plain}"
     else
-        echo -e "${red}x-ui 取消开机自启失败${plain}"
+        echo -e "${red}x-ui cancel boot failure ${plain}"
     fi
 
     if [[ $# == 0 ]]; then
@@ -277,7 +214,7 @@ migrate_v2_ui() {
 }
 
 install_bbr() {
-    bash <(curl -L -s https://raw.githubusercontent.com/sprov065/blog/master/bbr.sh)
+    bash &lt;(curl -L -s https://raw.githubusercontent.com/sprov065/blog/master/bbr.sh)
     echo ""
     before_show_menu
 }
@@ -286,11 +223,11 @@ update_shell() {
     wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/sprov065/x-ui/raw/master/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
-        echo -e "${red}下载脚本失败，请检查本机能否连接 Github${plain}"
+        echo -e "${red} failed to download the script, please check whether the machine can connect to Github${plain}"
         before_show_menu
     else
         chmod +x /usr/bin/x-ui
-        echo -e "${green}升级脚本成功，请重新运行脚本${plain}" && exit 0
+        echo -e "${green} upgrade script succeeded, please rerun script ${plain}" &amp;&amp; exit 0
     fi
 }
 
@@ -320,7 +257,7 @@ check_uninstall() {
     check_status
     if [[ $? != 2 ]]; then
         echo ""
-        echo -e "${red}面板已安装，请不要重复安装${plain}"
+        echo -e "${red} panel is installed, please do not install ${plain} again"
         if [[ $# == 0 ]]; then
             before_show_menu
         fi
@@ -334,7 +271,7 @@ check_install() {
     check_status
     if [[ $? == 2 ]]; then
         echo ""
-        echo -e "${red}请先安装面板${plain}"
+        echo -e "${red} please install the panel first ${plain}"
         if [[ $# == 0 ]]; then
             before_show_menu
         fi
@@ -348,15 +285,15 @@ show_status() {
     check_status
     case $? in
         0)
-            echo -e "面板状态: ${green}已运行${plain}"
+            echo -e "Panel Status: ${green} has run ${plain}"
             show_enable_status
             ;;
         1)
-            echo -e "面板状态: ${yellow}未运行${plain}"
+            echo -e "Panel Status: ${yellow} is not running ${plain}"
             show_enable_status
             ;;
         2)
-            echo -e "面板状态: ${red}未安装${plain}"
+            echo -e "Panel Status: ${red} not installed ${plain}"
     esac
     show_xray_status
 }
@@ -364,136 +301,79 @@ show_status() {
 show_enable_status() {
     check_enabled
     if [[ $? == 0 ]]; then
-        echo -e "是否开机自启: ${green}是${plain}"
+        echo -e "Whether it starts automatically at boot: ${green} is ${plain}"
     else
-        echo -e "是否开机自启: ${red}否${plain}"
+        echo -e "Whether it starts automatically at boot: ${red}No ${plain}"
     fi
 }
 
 check_xray_status() {
     count=$(ps -ef | grep "xray-linux" | grep -v "grep" | wc -l)
-    if [[ count -ne 0 ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-show_xray_status() {
-    check_xray_status
-    if [[ $? == 0 ]]; then
-        echo -e "xray 状态: ${green}运行${plain}"
-    else
-        echo -e "xray 状态: ${red}未运行${plain}"
-    fi
-}
-
-show_usage() {
-    echo "x-ui 管理脚本使用方法: "
-    echo "------------------------------------------"
-    echo "x-ui              - 显示管理菜单 (功能更多)"
-    echo "x-ui start        - 启动 x-ui 面板"
-    echo "x-ui stop         - 停止 x-ui 面板"
-    echo "x-ui restart      - 重启 x-ui 面板"
-    echo "x-ui status       - 查看 x-ui 状态"
-    echo "x-ui enable       - 设置 x-ui 开机自启"
-    echo "x-ui disable      - 取消 x-ui 开机自启"
-    echo "x-ui log          - 查看 x-ui 日志"
-    echo "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
-    echo "x-ui update       - 更新 x-ui 面板"
-    echo "x-ui install      - 安装 x-ui 面板"
-    echo "x-ui uninstall    - 卸载 x-ui 面板"
-    echo "------------------------------------------"
-}
-
-show_menu() {
-    echo -e "
-  ${green}x-ui 面板管理脚本${plain}
---- https://blog.sprov.xyz/x-ui ---
-  ${green}0.${plain} 退出脚本
-————————————————
-  ${green}1.${plain} 安装 x-ui
-  ${green}2.${plain} 更新 x-ui
-  ${green}3.${plain} 卸载 x-ui
-————————————————
-  ${green}4.${plain} 重置用户名密码
-  ${green}5.${plain} 重置面板设置
-  ${green}6.${plain} 设置面板端口
-————————————————
-  ${green}7.${plain} 启动 x-ui
-  ${green}8.${plain} 停止 x-ui
-  ${green}9.${plain} 重启 x-ui
- ${green}10.${plain} 查看 x-ui 状态
- ${green}11.${plain} 查看 x-ui 日志
-————————————————
- ${green}12.${plain} 设置 x-ui 开机自启
- ${green}13.${plain} 取消 x-ui 开机自启
-————————————————
- ${green}14.${plain} 一键安装 bbr (最新内核)
- "
-    show_status
-    echo && read -p "请输入选择 [0-14]: " num
+    if [[ count 
+    
+    us
+    echo &amp;&amp; read -p "请输入选择 [0-14]: " num
 
     case "${num}" in
         0) exit 0
         ;;
-        1) check_uninstall && install
+        1) check_uninstall &amp;&amp; install
         ;;
-        2) check_install && update
+        2) check_install &amp;&amp; update
         ;;
-        3) check_install && uninstall
+        3) check_install &amp;&amp; uninstall
         ;;
-        4) check_install && reset_user
+        4) check_install &amp;&amp; reset_user
         ;;
-        5) check_install && reset_config
+        5) check_install &amp;&amp; reset_config
         ;;
-        6) check_install && set_port
+        6) check_install &amp;&amp; set_port
         ;;
-        7) check_install && start
+        7) check_install &amp;&amp; start
+       https://accounts.google.com/b/0/AddMailService ;;
+8) check_install &amp;&amp; stop
         ;;
-        8) check_install && stop
+        9) check_install &amp;&amp; restart
         ;;
-        9) check_install && restart
+        10) check_install &amp;&amp; status
         ;;
-        10) check_install && status
+        11) check_install &amp;&amp; show_log
         ;;
-        11) check_install && show_log
+        12) check_install &amp;&amp; enable
         ;;
-        12) check_install && enable
-        ;;
-        13) check_install && disable
+        13) check_install &amp;&amp; disable
         ;;
         14) install_bbr
         ;;
-        *) echo -e "${red}请输入正确的数字 [0-14]${plain}"
+        *) echo -e "${red}Please enter the correct number [0-14]${plain}"
         ;;
     esac
 }
 
 
-if [[ $# > 0 ]]; then
+if [[ $# &gt; 0 ]]; then
     case $1 in
-        "start") check_install 0 && start 0
+        "start") check_install 0 &amp;&amp; start 0
         ;;
-        "stop") check_install 0 && stop 0
+        "stop") check_install 0 &amp;&amp; stop 0
         ;;
-        "restart") check_install 0 && restart 0
+        "restart") check_install 0 &amp;&amp; restart 0
         ;;
-        "status") check_install 0 && status 0
+        "status") check_install 0 &amp;&amp; status 0
         ;;
-        "enable") check_install 0 && enable 0
+        "enable") check_install 0 &amp;&amp; enable 0
         ;;
-        "disable") check_install 0 && disable 0
+        "disable") check_install 0 &amp;&amp; disable 0
         ;;
-        "log") check_install 0 && show_log 0
+        "log") check_install 0 &amp;&amp; show_log 0
         ;;
-        "v2-ui") check_install 0 && migrate_v2_ui 0
+        "v2-ui") check_install 0 &amp;&amp; migrate_v2_ui 0
         ;;
-        "update") check_install 0 && update 0
+        "update") check_install 0 &amp;&amp; update 0
         ;;
-        "install") check_uninstall 0 && install 0
+        "install") check_uninstall 0 &amp;&amp; install 0
         ;;
-        "uninstall") check_install 0 && uninstall 0
+        "uninstall") check_install 0 &amp;&amp; uninstall 0
         ;;
         *) show_usage
     esac
